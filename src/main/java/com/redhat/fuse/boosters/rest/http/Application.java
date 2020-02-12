8b1fdce917c960c68b894e212bd8332ee15ee909
Doobri <doobri68@gmail.com>
2020-02-12 14:35:57 +0000

    Updated App.java with string msg

--- src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
+++ src/main/java/com/redhat/fuse/boosters/rest/http/Application.java
@@ -10,6 +10,9 @@ public class Application {
      * Main method to start the application.
      */
     public static void main(String[] args) {
+        String doNothing = "Hello World!";
+        doNothing += " A useless string!";
+        System.out.println(doNothing);
         SpringApplication.run(Application.class, args);
     }
 
